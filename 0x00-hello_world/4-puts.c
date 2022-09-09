7f6e361a5949b98ffcd927e0f29dcadef8aec19b
Sipho <laymansimlo@icloud.com>
2022-09-08 18:40:06 -0700

    put and printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
- *main entry point
- *Return Always 0(success)
+ * main entry point
+ * Return: Always 0(success)
  *
- *
+ * puts prints out the string function
 */
 int main(void)
-{puts ("\"Programming is like building a multilingual puzzle"); return(0);
+{
+	puts("\"Programming is like building a multilingual puzzle"); return(0);
 }
