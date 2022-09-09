5d69b43b6312796e286ba09aedccd434e2a2d4f0
Sipho <laymansimlo@icloud.com>
2022-09-08 19:58:47 -0700

    put and printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
 (* main - Entry point 
- \* printf prints string
- \* Return: always 0(Success)
- \* \n introduces new line break)*  
- */
+* printf prints string
+* Return: always 0(Success))*
+* \n introduces new line break  
+*/
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n"); 
