8c957174a1ce9ab4c185d79a64ceed2e414c93de
Sipho <laymansimlo@icloud.com>
2022-09-10 10:05:52 -0700

    data type sizes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,17 +4,17 @@
  * Define data ans their size
  * Return: alwys 0 (Success)
  */
-int main (void)
+int main(void)
 {
 	char char_size;
 	float fl;
 	int i;
 	long int lint;
 	long long int lo;
-	printf("Size of a char: %ld byte\n",sizeof(char_size));
-	printf("Size of a float: %ld byte\n",sizeof(fl));
-	printf("Size of an int: %ld byte\n",sizeof(i));
-	printf("Size of a long int: %ld byte\n",sizeof(lint));
-	printf("Size of a long long int: %ld bytes\n",sizeof(lo));
-	return(0);
+	printf("Size of a char: %ld byte\n", sizeof(char_size));
+	printf("Size of a float: %ld byte\n", sizeof(fl));
+	printf("Size of an int: %ld byte\n", sizeof(i));
+	printf("Size of a long int: %ld byte\n", sizeof(lint));
+	printf("Size of a long long int: %ld bytes\n", sizeof(lo));
+	return (0);
 }
