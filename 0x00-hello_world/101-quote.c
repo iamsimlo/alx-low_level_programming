614618f921dd8849944d6a0c1fa90d9209a5759f
Sipho <laymansimlo@icloud.com>
2022-09-11 11:06:19 -0700

    Error reporting

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+/**
+ * main - Entry point
+ * Hello World for Error Codes
+ * Return 1(errors)
+ * Return Always 0(Success)
+ */
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\"-Dora Korpar,2015-10-19\n");
+	return (1);
+}
