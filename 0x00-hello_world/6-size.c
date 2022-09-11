3879c5e30f167774c659ade797fc475f8c1732c6
Sipho <laymansimlo@icloud.com>
2022-09-11 08:18:15 -0700

    Data type siezes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,16 +6,16 @@
  */
 int main(void)
 {
-	char char_size;
+	char c;
 	float fl;
 	int i;
 	long int lint;
 	long long int lo;
 	/*declarations of data types above*/
-	printf("Size of a char: %ld byte(s)\n", sizeof(char_size));
-	printf("Size of a float: %ld byte(s)\n", sizeof(fl));
-	printf("Size of an int: %ld byte(s)\n", sizeof(i));
-	printf("Size of a long int: %ld byte(s)\n", sizeof(lint));
-	printf("Size of a long long int: %ld bytes(s)\n", sizeof(lo));
+	printf("Size of a char: %ld byte\(s\)\n", sizeof(c));
+	printf("Size of a float: %ld byte\(s\)\n", sizeof(fl));
+	printf("Size of an int: %ld byte\(s\)\n", sizeof(i));
+	printf("Size of a long int: %ld byte\(s\)\n", sizeof(lint));
+	printf("Size of a long long int: %ld bytes\(s\)\n", sizeof(lo));
 	return (0);
 }
