7254df699814124783c22f883fc36f60cb651b14
Sipho <laymansimlo@icloud.com>
2022-09-11 10:24:46 -0700

    datatypes size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,14 +9,14 @@ int main(void)
 	/*Declaring variables*/
 	char c;
 	int i;
-	long li;
-	long long lli;
+	long lint;
+	long long loli;
 	float f;
 
 	printf("Size of a char: %ld byte(s)\n", sizeof(c));
 	printf("Size of an int: %ld byte(s)\n", sizeof(i));
-	printf("Size of a long int: %ld byte(s)\n", sizeof(li));
-	printf("Size of a long long int: %ld byte(s)\n", sizeof(lli));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(lint));
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(loli));
 	printf("Size of a float: %ld byte(s)\n", sizeof(f));
 	return (0);
 }
