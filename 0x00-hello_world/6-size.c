2dcb565aee750b7a89c12df358ce8339235a72f8
Sipho <laymansimlo@icloud.com>
2022-09-11 08:48:57 -0700

    intel error

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,10 +12,10 @@ int main(void)
 	long int lint;
 	long long int lo;
 	/*declarations of data types above*/
-	printf("Size of a char: %ld byte\(s\)\n", sizeof(c));
-	printf("Size of a float: %ld byte\(s\)\n", sizeof(fl));
-	printf("Size of an int: %ld byte\(s\)\n", sizeof(i));
-	printf("Size of a long int: %ld byte\(s\)\n", sizeof(lint));
-	printf("Size of a long long int: %ld bytes\(s\)\n", sizeof(lo));
+	printf("Size of a char: %ld byte(s)\n", sizeof(c));
+	printf("Size of a float: %ld byte(s)\n", sizeof(fl));
+	printf("Size of an int: %ld byte(s)\n", sizeof(i));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(lint));
+	printf("Size of a long long int: %ld bytes(s)\n", sizeof(lo));
 	return (0);
 }
