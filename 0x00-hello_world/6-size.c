2a3b49beb22adc50430a6a3b6c6171b154dc3ae5
Sipho <laymansimlo@icloud.com>
2022-09-11 10:21:28 -0700

    dt edit 1

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,16 +6,17 @@
  */
 int main(void)
 {
+	/*Declaring variables*/
 	char c;
-	float fl;
 	int i;
-	long lint;
-	long long lo;
-	/*declarations of data types above*/
+	long li;
+	long long lli;
+	float f;
+
 	printf("Size of a char: %ld byte(s)\n", sizeof(c));
-	printf("Size of a float: %ld byte(s)\n", sizeof(fl));
 	printf("Size of an int: %ld byte(s)\n", sizeof(i));
-	printf("Size of a long int: %ld byte(s)\n", sizeof(lint));
-	printf("Size of a long long int: %ld bytes(s)\n", sizeof(lo));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(li));
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(lli));
+	printf("Size of a float: %ld byte(s)\n", sizeof(f));
 	return (0);
 }
