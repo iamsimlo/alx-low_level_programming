3160ee4a047677e107af97494e8aafb29b44bf0f
Sipho <laymansimlo@icloud.com>
2022-09-12 01:59:54 -0700

    hello errors

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /**
- * main - Entry point
- * Hello World for Error Codes
- * Return 1(errors)
- * Return Always 0(Success)
+ * main - must return error
+ *
+ *
+ * Return Always 1(Success)
  */
-int main(void)
+int main(void) /*main function*/
 {
-	fprintf(stderr, "and that piece of art is useful\"-Dora Korpar,2015-10-19\n");
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
 }
