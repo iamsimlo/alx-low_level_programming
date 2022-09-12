687a841bf4fb1dc4451fe1363dea2d69afbc1c6d
Sipho <laymansimlo@icloud.com>
2022-09-12 12:28:22 -0700

    Print error command

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
- * main -must return error
- *
- *
- * Return Always 1(Success)
+ * main - Entry point
+ * stderr = 2
+ * Return: always 1(success)
  */
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
