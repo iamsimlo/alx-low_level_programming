662824e2a9824f131992375f8822576d0a339583
Sipho <laymansimlo@icloud.com>
2022-09-15 13:06:46 -0700

    first 10 single digits

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Program print all single digits from 0-10
+ *
+ * Return: always 0(success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+}
