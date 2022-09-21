5ae87e75eb626e2ada7f3f2cc49c61c8bb2af394
Sipho <laymansimlo@icloud.com>
2022-09-21 16:15:05 -0700

    function that concatenates two strings.

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,5 +1,8 @@
 #include "main.h"
 /**
+ * _strcat -this function appends the src string to the dest string,
+ *		 overwriting the terminating null byte (\0) at the end of dest
+ *		 and then adds a terminating null byte
  * @dest: A pointer to the string to be concatenated upon.
  * @src: The source string to be appended to dest.
  *
