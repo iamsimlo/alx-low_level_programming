c9521e9fe4a3f9e575710de68510fd154bef5b08
Sipho <laymansimlo@icloud.com>
2022-10-08 09:06:35 -0700

    print most numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_most_numbers - ommits num 2 & 4
+ * @i is the parameter
+ * Return: is always 0
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		if (i != 2 && i != 4)
+			_putchar('0' + i);
+	}
+	_putchar('\n');
+}
